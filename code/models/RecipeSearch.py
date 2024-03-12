35a4389565d49df81c20f38e1a42a1c3bd9275f8
0ctopusHead <yanwarut_s@cmu.ac.th>
2024-03-12 23:17:00 +0700

    added BM25 & ranking score by query

---
+++ code/models/RecipeSearch.py
@@ -0,0 +1,63 @@
+import os
+import pickle
+from spellchecker import SpellChecker
+import re
+from BM25 import BM25
+from nltk.corpus import stopwords
+from sklearn.feature_extraction.text import TfidfVectorizer
+from pathlib import Path
+from nltk.tokenize import word_tokenize
+
+
+checker = SpellChecker(language='en')
+
+stored_folder = Path(os.path.abspath('')).parent.parent / "data" / "processed" / "cleaned_df.pkl"
+recipe = pickle.load(open(stored_folder, 'rb'))
+
+
+def preProcess(s):
+    s = re.sub(r'[^A-Za-z]', ' ', s)
+    s = re.sub(r'\s+', ' ', s)
+    s = word_tokenize(s)
+    text = ' '.join(s)
+    return text
+
+
+class ManualIndexer:
+    def __init__(self, recipe_data):
+        self.recipe_data = recipe_data
+        self.stored_folder = Path(os.path.abspath('')).parent.parent / "data" / "model/"
+        self.stored_file = 'manual_indexer.pkl'
+        if os.path.isfile(self.stored_folder / self.stored_file):
+            with open(self.stored_folder / self.stored_file, 'rb') as f:
+                cache_dict = pickle.load(f)
+                print(cache_dict)
+            self.__dict__.update(cache_dict)
+        else:
+            self.run_indexer()
+
+    def run_indexer(self):
+        text_columns = ['Name', 'RecipeIngredientParts', 'RecipeInstructions']
+        self.df = self.recipe_data[text_columns]
+
+        self.tfidf_vectorizer = TfidfVectorizer(preprocessor=preProcess, stop_words=stopwords.words('english'))
+        self.bm25 = BM25(self.tfidf_vectorizer)
+
+        self.bm25.fit(self.df.apply(lambda row: ' '.join(row), axis=1))
+
+        with open(self.stored_folder / self.stored_file, 'wb') as f:
+            pickle.dump(self.__dict__, f)
+
+    def query(self, q):
+        return_score_list = self.bm25.transform(q)
+        hit = (return_score_list > 0).sum()
+        rank = return_score_list.argsort()[::-1][:hit]
+        results = self.df.iloc[rank].copy().reset_index(drop=True)
+        results['score'] = return_score_list[rank]
+        return results
+
+
+if __name__ == '__main__':
+    manual_indexer = ManualIndexer(recipe)
+    query_res = manual_indexer.query("crab")
+    print(query_res)
